56ad0949e0482be5208ca40579133db9222e5dbc
Paulo Matheus Ferrari <paulomferrari26@gmail.com>
2024-01-24 12:29:59 -0300

    Aula 3

--- escola/views.py
+++ escola/views.py
@@ -1,6 +1,15 @@
-from django.http import JsonResponse
+from rest_framework import viewsets
+from escola.models import Aluno, Curso
+from serializer import AlunoSerializer, CursoSerializer
 
-def alunos(request):
-    if request.method == 'GET':
-        aluno = {'id':1, 'nome':'Guilherme'}
-        return JsonResponse(aluno)
+class AlunoViewSet(viewsSet.ModelViewSet):
+    """Exibindo todos os alunos e alunas"""
+    queryset = Aluno.objects.all()
+    serializer_class = AlunoSerializer
+
+class CursoViewSet(viewSet.ModelViewSet):
+    """Exibindo cursos"""
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
+    
+
